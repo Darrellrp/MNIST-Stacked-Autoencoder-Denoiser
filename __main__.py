03cee533d78614653ecf4148731b02bb9f4c4dad
Darrell Poleon <darrellpoleon@Darrells-MacBook-Pro.local>
2019-08-16 19:06:42 +0200

    MNIST-Stacked-Autoencoder 1.0

--- __main__.py
+++ __main__.py
@@ -1,19 +1,43 @@
-import tensorflow as tf
-from keras.layers import Dense
-import matplotlib.pyplot as plt
-from keras.models import Sequential
-import numpy as np
 import random
+from time import time
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from keras import optimizers
+from keras.layers import Dense, Input
+from keras.models import Sequential, Model
+from keras.callbacks import TensorBoard
 
+# Input 
+MNIST_IMG_DIM = [28, 28]
+
+# Autocoder
+HIDDEN_SIZE = 128
+CODE_SIZE = 32
+
+# Training
+EPOCHS = 5
+BATCH_SIZE = 32
+VERBOSE = 1
+
+# Optimization
+LEARNING_RATE = 0.001
+BETA_1 = 0.9
+BETA_2 = 0.999
+EPSILON = None
+DECAY = 0.0
+AMSGRAD = False
+
+# Post Training
 EVALUATE = False
 PREDICT = True
+NUMBER_SHOWN_OF_PREDICTIONS = 4
 
 # Load mnist dataset and generate train & test dataset
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
 x_train_shape = x_train.shape[0]
-input_shape = [28, 28]
-input_size = np.prod(input_shape)
+input_size = np.prod(MNIST_IMG_DIM)
 
 # Reshape datasets from (_, 28, 28) to (_, 784)
 x_train = x_train.reshape(x_train.shape[0], input_size)
@@ -27,23 +51,36 @@ x_test = np.true_divide(x_test, 255)
 x_train = x_train.astype('float32')
 x_test = x_test.astype('float32')
 
-hidden_size = 128
-code_size = 32
+# Input layer (size=748)
+input_img = Input(shape=(input_size, ))
 
-autoencoder = Sequential()
+# Autoencoder 1.
+hidden_1 = Dense(HIDDEN_SIZE, activation='relu')(input_img)
+code_1 = (Dense(CODE_SIZE, activation='relu'))(hidden_1)
 
-# Input layer (size=748) & Hidden layer 1
-autoencoder.add(Dense(hidden_size, activation='relu', input_dim=input_size))
-# Code layer
-autoencoder.add(Dense(code_size, activation='relu'))
-# Hidden layer 2
-autoencoder.add(Dense(hidden_size, activation='relu'))
+# Autoencoder 2.
+hidden_2 = Dense(HIDDEN_SIZE, activation='relu')(code_1)
+code_2 = (Dense(CODE_SIZE, activation='relu'))(hidden_2)
+
+# Autoencoder 3.
+hidden_3 = Dense(HIDDEN_SIZE, activation='relu')(code_2)
+code_3 = (Dense(CODE_SIZE, activation='relu'))(hidden_3)
+
+# Autoencoder 2.
+hidden_4 = Dense(HIDDEN_SIZE, activation='relu')(code_3)
+code_4 = (Dense(CODE_SIZE, activation='relu'))(hidden_4)
+
 # Output layer (Reconstructed image, siz=748)
-autoencoder.add(Dense(input_size, activation='sigmoid'))
+output_img = Dense(input_size, activation='sigmoid')(code_4)
 
-autoencoder.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+autoencoder = Model(input_img, output_img)
 
-autoencoder.fit(x_train, x_train, epochs=5, batch_size=32, verbose=1)
+adam = optimizers.Adam(lr=LEARNING_RATE, beta_1=BETA_1, beta_2=BETA_2, epsilon=EPSILON, decay=DECAY, amsgrad=AMSGRAD)
+autoencoder.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
+
+tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+
+autoencoder.fit(x_train, x_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE, callbacks=[tensorboard])
 
 if EVALUATE:
 	score, accuracy = autoencoder.evaluate(x_test, x_test)
@@ -54,10 +91,8 @@ if PREDICT:
 	reconstructed = autoencoder.predict(x_test)
 
 	# Reshape datasets from (_, 784) to (_, 28, 28)
-	reconstructed = reconstructed.reshape(x_test.shape[0], input_shape[0], input_shape[1])
-	actual = x_test.reshape(x_test.shape[0], input_shape[0], input_shape[1])
-
-	NUMBER_SHOWN_OF_PREDICTIONS = 4
+	reconstructed = reconstructed.reshape(x_test.shape[0], MNIST_IMG_DIM[0], MNIST_IMG_DIM[1])
+	actual = x_test.reshape(x_test.shape[0], MNIST_IMG_DIM[0], MNIST_IMG_DIM[1])
 
 	# Show a number of predictions
 	for i in range(NUMBER_SHOWN_OF_PREDICTIONS):
