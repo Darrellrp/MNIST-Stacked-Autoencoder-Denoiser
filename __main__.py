3274869c948cdb5c58645638fc785dc6f5a76982
Darrell Poleon <darrellpoleon@Darrells-MacBook-Pro.local>
2019-08-17 08:33:20 +0200

    WSAEs-LSTM-Price-Predictor 1.0

--- __main__.py
+++ __main__.py
@@ -1,26 +1,40 @@
 import random
+import quandl
+import csv
+import os.path
 from time import time
 import numpy as np
 import tensorflow as tf
+import pandas as pd
 import matplotlib.pyplot as plt
 from keras import optimizers
 from keras.layers import Dense, Input
 from keras.models import Sequential, Model
 from keras.callbacks import TensorBoard
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from alpha_vantage.timeseries import TimeSeries
 
-# Input 
-MNIST_IMG_DIM = [28, 28]
+# Data Source
+AV_API_KEY = 'OHL1UQBAF94QK9FH'
+DIR_RAW_DATA = 'data/alpha-vantage/raw'
+DIR_RAW_CLEAN = 'data/alpha-vantage/clean'
+FILE_PATH_RAW_DATA = DIR_RAW_DATA + '/{}_daily_full.csv'
+FILE_PATH_CLEAN_DATA = DIR_RAW_CLEAN + '/{}_daily_full_{}.csv'
+
+# Pre processing
+TRAIN_TEST_SPLIT = 0.8
 
 # Autocoder
-HIDDEN_SIZE = 128
-CODE_SIZE = 32
+HIDDEN_SIZE = 10
+CODE_SIZE = 6
 
 # Training
-EPOCHS = 5
+EPOCHS = 20
 BATCH_SIZE = 32
 VERBOSE = 1
 
-# Optimization
+# Optimization Neural Network
 LEARNING_RATE = 0.001
 BETA_1 = 0.9
 BETA_2 = 0.999
@@ -28,72 +42,173 @@ EPSILON = None
 DECAY = 0.0
 AMSGRAD = False
 
+# Compilation Neural Network
+LOSS = 'mean_squared_error'
+OPTIMIZER = 'adam'
+METRICS = ['accuracy']
+
+
+# Tensorflow
+FILE_PATH_TF_LOGS = 'logs/{}'
+
 # Post Training
 EVALUATE = False
 PREDICT = True
 NUMBER_SHOWN_OF_PREDICTIONS = 4
 
-# Load mnist dataset and generate train & test dataset
-(x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
 
-x_train_shape = x_train.shape[0]
-input_size = np.prod(MNIST_IMG_DIM)
+# Read market items from text file
+market_items = open('market-items.txt').readline().split(', ')
 
-# Reshape datasets from (_, 28, 28) to (_, 784)
-x_train = x_train.reshape(x_train.shape[0], input_size)
-x_test = x_test.reshape(x_test.shape[0], input_size)
+# Remove newline character from market items
+market_items = [m.replace('\n', '') for m in market_items]
 
-# Normalize RGB-values
-x_train = np.true_divide(x_train, 255)
-x_test = np.true_divide(x_test, 255)
+data = None
+targets = None
 
-# Convert to float32
-x_train = x_train.astype('float32')
-x_test = x_test.astype('float32')
+# if clean data or targets doesn't exist, clean and store again
+if not os.path.exists('{}/{}'.format(DIR_RAW_CLEAN, 'data_collection.csv')) \
+		or not os.path.exists('{}/{}'.format(DIR_RAW_CLEAN, 'targets_collection.csv')):
 
+	# ***************************************     Retrieve financial data     ***************************************
+	for market_item_name in market_items:
+		file_path_raw = FILE_PATH_RAW_DATA.format(market_item_name)
+
+		if not os.path.exists(file_path_raw):
+			print('Fetching Daily Time Series -csv (Alpha Vantage) - {}...'.format(market_item_name))
+			ts = TimeSeries(key=AV_API_KEY, output_format='pandas', indexing_type='date')
+
+			# Get json object with the intraday data and another with  the call's metadata
+			market_item, meta_data = ts.get_daily(market_item_name, outputsize='full')
+
+			print(market_item_name)
+			print('Data size: {}'.format(market_item.shape[0]))
+			print('')
+
+			rename_columns = {'1. open': 'open', '2. high': 'high', '3. low': ' low',
+							  '4. close': 'close', '5. volume': 'volume'}
+
+			# Rename columns
+			market_item.rename(columns=rename_columns, inplace=True)
+			market_item.index.name = 'timestamp'
+
+			# Store Market item data
+			market_item.to_csv(file_path_raw, header=True, float_format='%.3f')
+
+		else:
+			print('Loading Daily Time Series -csv (Alpha Vantage) - {}...'.format(market_item_name))
+			market_item = pd.read_csv(file_path_raw, parse_dates=['timestamp'])
+
+		market_item['close'].plot()
+		plt.title(market_item_name)
+		# plt.show()
+
+	# ***************************************     Pre-processing     ***************************************
+		file_path_raw = FILE_PATH_RAW_DATA.format(market_item_name)
+		file_path_clean_data = FILE_PATH_CLEAN_DATA.format(market_item_name, 'data')
+		file_path_clean_targets = FILE_PATH_CLEAN_DATA.format(market_item_name, 'targets')
+
+		# if clean data or targets doesn't exist, clean and store again
+		if not os.path.exists(file_path_clean_data) or not os.path.exists(file_path_clean_targets):
+			print('Pre-processing {}...'.format(market_item_name))
+
+			# Remove last row, Drop timestamp column
+			market_item_data = market_item.drop(market_item.tail(1).index).drop('timestamp', axis=1).to_numpy()
+
+			# Select close columns, Drop First row (Next days close price)
+			market_item_targets = market_item['close'].drop(market_item.head(1).index).to_numpy()
+
+			print('')
+
+			scaler = MinMaxScaler(feature_range=(0, 1))
+			scaler = scaler.fit(market_item_data)
+
+			# Normalize financial data
+			market_item_data = scaler.transform(market_item_data)
+
+			# Convert to float32
+			market_item_data = market_item_data.astype('float32')
+			market_item_targets = market_item_targets.astype('float32')
+
+			# Store cleaned financial data
+			np.savetxt(FILE_PATH_CLEAN_DATA.format(market_item_name, 'data'), market_item_data, delimiter=',')
+			np.savetxt(FILE_PATH_CLEAN_DATA.format(market_item_name, 'targets'), market_item_targets, delimiter=',')
+
+		else:
+			# Read cleaned financial data
+			market_item_data = pd.read_csv(FILE_PATH_CLEAN_DATA.format(market_item_name, 'data'))
+			market_item_targets = pd.read_csv(FILE_PATH_CLEAN_DATA.format(market_item_name, 'targets'))
+
+		if data is None:
+			data = market_item_data
+			targets = market_item_targets
+		else:
+			data = np.concatenate((data, market_item_data))
+			targets = np.concatenate((targets, market_item_targets))
+
+	np.savetxt('{}/{}'.format(DIR_RAW_CLEAN, 'data_collection.csv'), data, delimiter=',')
+	np.savetxt('{}/{}'.format(DIR_RAW_CLEAN, 'targets_collection.csv'), targets, delimiter=',')
+
+else:
+	print('Loading Collection -csv (Alpha Vantage)...')
+	data = pd.read_csv('{}/{}'.format(DIR_RAW_CLEAN, 'data_collection.csv')).values
+	targets = pd.read_csv('{}/{}'.format(DIR_RAW_CLEAN, 'targets_collection.csv')).values
+
+print('')
+
+# Train Test Splits
+print('Generating Train Test split...')
+x_train, x_test, y_train, y_test = train_test_split(data, targets, test_size=0.2, random_state=42)
+
+print('Number of rows train data: {}'.format(len(x_train)))
+print('Number of rows test data: {} '.format(len(x_test)))
+print('')
+
+input_size = x_train.shape[1]
+
+# ***************************************     Neural Network Architecture     ***************************************
 # Input layer (size=748)
-input_img = Input(shape=(input_size, ))
+input_img = Input(shape=(input_size,))
 
 # Autoencoder 1.
 hidden_1 = Dense(HIDDEN_SIZE, activation='relu')(input_img)
-code_1 = (Dense(CODE_SIZE, activation='relu'))(hidden_1)
+code_1 = Dense(CODE_SIZE, activation='relu')(hidden_1)
 
 # Autoencoder 2.
 hidden_2 = Dense(HIDDEN_SIZE, activation='relu')(code_1)
-code_2 = (Dense(CODE_SIZE, activation='relu'))(hidden_2)
+code_2 = Dense(CODE_SIZE, activation='relu')(hidden_2)
 
 # Autoencoder 3.
 hidden_3 = Dense(HIDDEN_SIZE, activation='relu')(code_2)
-code_3 = (Dense(CODE_SIZE, activation='relu'))(hidden_3)
+code_3 = Dense(CODE_SIZE, activation='relu')(hidden_3)
 
 # Autoencoder 2.
 hidden_4 = Dense(HIDDEN_SIZE, activation='relu')(code_3)
-code_4 = (Dense(CODE_SIZE, activation='relu'))(hidden_4)
+code_4 = Dense(CODE_SIZE, activation='relu')(hidden_4)
 hidden_5 = Dense(HIDDEN_SIZE, activation='relu')(code_4)
 
 # Output layer (Reconstructed image, siz=748)
 output_img = Dense(input_size, activation='sigmoid')(hidden_5)
 
-autoencoder = Model(input_img, output_img)
+stacked_autoencoder = Model(input_img, output_img)
 
 adam = optimizers.Adam(lr=LEARNING_RATE, beta_1=BETA_1, beta_2=BETA_2, epsilon=EPSILON, decay=DECAY, amsgrad=AMSGRAD)
-autoencoder.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
+# optimizers: 'adadelta' | loss: 'mape', 'binary_crossentropy'
+stacked_autoencoder.compile(optimizer=OPTIMIZER, loss=LOSS, metrics=METRICS)
 
-tensorboard = TensorBoard(log_dir="logs/{}".format(time()))
+tensorboard = TensorBoard(log_dir=FILE_PATH_TF_LOGS.format(time()))
 
-autoencoder.fit(x_train, x_train, epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=VERBOSE, callbacks=[tensorboard])
+stacked_autoencoder.fit(x_train, x_train, epochs=EPOCHS, batch_size=BATCH_SIZE,
+						verbose=VERBOSE, callbacks=[tensorboard])
 
+# ***************************************     Post Training     ***************************************
 if EVALUATE:
-	score, accuracy = autoencoder.evaluate(x_test, x_test)
+	score, accuracy = stacked_autoencoder.evaluate(x_test, x_test)
 	print('Test score:', score)
 	print('Test accuracy:', accuracy)
 
 if PREDICT:
-	reconstructed = autoencoder.predict(x_test)
-
-	# Reshape datasets from (_, 784) to (_, 28, 28)
-	reconstructed = reconstructed.reshape(x_test.shape[0], MNIST_IMG_DIM[0], MNIST_IMG_DIM[1])
-	actual = x_test.reshape(x_test.shape[0], MNIST_IMG_DIM[0], MNIST_IMG_DIM[1])
+	reconstructed = stacked_autoencoder.predict(x_test)
 
 	# Show a number of predictions
 	for i in range(NUMBER_SHOWN_OF_PREDICTIONS):
@@ -103,12 +218,6 @@ if PREDICT:
 		print('')
 
 		# Print actual label
-		print('The label is %d' % y_test[rand_i])
+		print('The X test: {}'.format(x_test[rand_i]))
+		print('The reconstructed test: {}'.format(reconstructed[rand_i]))
 
-		# Show actual image
-		plt.imshow(actual[rand_i], cmap='Greys')
-		plt.show()
-
-		# Show reconstructed image
-		plt.imshow(reconstructed[rand_i], cmap='Greys')
-		plt.show()
